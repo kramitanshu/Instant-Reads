aa58f850a1ee531a6ade12e6a7e29784d5dd1efc
Kumar Amitanshu Singh <kumaramitanshu2@gmail.com>
2024-03-11 00:31:32 +0530

    Delete button working

--- chromeExtension.js
+++ chromeExtension.js
@@ -1,25 +1,15 @@
 let myLeads = [];
-let oldLeads = [];
-const inputBtn = document.getElementById("input-btn")
+
 const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const tabBtn = document.getElementById("tab-btn")
 const deleteBtn = document.getElementById("delete-btn")
+const markedDeleteBtn = document.getElementById("marked-delete-btn")
 // const leadEl = document.getElementById("leads-el")
 const ulEl = document.getElementById("ul-el")
-const tabBtn = document.getElementById("tab-btn")
-
-
-// console.log(deleteBtn)
-// console.log(ulEl)
 
 // Getting leads from localstorage
-// localStorage.clear()
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myleads"));
-// console.log(leadsFromLocalStorage);
-
-// const tabs = [
-// 	{ url :"https://codeforces.com/"}
-// ]
-
 
 tabBtn.addEventListener("click", function () {
 
@@ -31,13 +21,11 @@ tabBtn.addEventListener("click", function () {
 	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 		// console.log(tabs);
 		const data = tabs[0].url;
-		myLeads.push(data);
+		const newRead = {data, checked: false}
+		myLeads.push(newRead);
 		localStorage.setItem("myleads", JSON.stringify(myLeads));
 		render(myLeads);
 	})
-
-	// console.log(tabs[0].url);
-
 })
 
 if (leadsFromLocalStorage) {
@@ -48,34 +36,26 @@ if (leadsFromLocalStorage) {
 function render(leads) {
 	let listItems = "";
 	for (let i = 0; i < leads.length; i++){
+		
 		// listItems += "<li><a target='_blank' href='" + myLeads[i] + "'> " + myLeads[i] + " </a></li>";   // ----------- Complecated string
-		listItems +=
-			`
-				<li>
-					<a  href="${leads[i]}" target="_blank">
-						${leads[i]}
-					</a>
-				</li>
-			`
+
+		// here I need to add check box, and event listener on check box, so that it modifies it's value
+		if(!leads[i].checked){
+			listItems +=
+				`
+					<li>
+						<a  href="${leads[i].value}" target="_blank">
+							${leads[i].value}
+						</a>
+					</li>
+				`
+
+		}
 		// console.log(listItems);
 
 	}
 	ulEl.innerHTML = listItems;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Render the leads in the unordered list using ulEL.textcontent
 
@@ -88,7 +68,10 @@ function render(leads) {
 
 	// This one is good !!
 	inputBtn.addEventListener("click", function () {
-		let lead = inputEl.value;
+		let value = inputEl.value;
+		// technically alert
+		if(value === "") return  
+		let lead = {value, checked: false};
 		myLeads.push(lead);
 		console.log(myLeads);
 		// Clear out the input field
@@ -112,19 +95,36 @@ function render(leads) {
 			// 	ulEl.textContent += myLeads[i]  + " ";
 	// }
 // }
-
-
-
-
-
 // Listen double click on the delete button
 // when clicked, clear localstorage, myLeads and DOM
 deleteBtn.addEventListener("dblclick", function () {
 	localStorage.clear()
-	myLeads = ""
+	myLeads = []
 	render(myLeads);
 	// console.log("double click");
 })
+
+markedDeleteBtn.addEventListener("click", (e) =>{
+	e.preventDefault();
+	console.log("button clicked")
+	// const data = JSON.parse(localStorage.getItem("myLeads"));
+	const modifiedLead = [];
+	// localStorage.clear();
+	// myLeads = [];
+
+	for(let i = 0; i<myLeads.length; i++){
+		if(!myLeads[i].checked){
+			modifiedLead.push(myLeads[i]);
+		}
+	}
+	myLeads = modifiedLead;
+	localStorage.clear();
+	localStorage.setItem("myleads", JSON.stringify(myLeads))
+	console.log(myLeads);
+	render(myLeads);
+})
+
+render(myLeads)
 
 
 // let listItems = "";
